81667227d0e348d5ef95d8a886cca9da30c0559c
WhoThat <benhuff322@gmail.com>
2015-04-05 12:14:16 -0700

    Revert "attempt 4 to fix foundation"
    This reverts commit 5529ddd52b8845e4a1f76d7d015506a68743cf19.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,13 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require foundation
+// require foundation
 //= require turbolinks
 //= require_tree .
 
-var ready;
-ready = function() {
-  $(document).foundation();
-};
-$(document).ready(ready);
-$(document).on('page:load', ready);
+ //$(function(){ $(document).foundation(); });
